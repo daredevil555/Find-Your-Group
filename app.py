7789599d3de7276c2886866b9638c020efe61984
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-06-09 11:20:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+with open('model.pickle','rb') as file:
+    mp = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    output = mp.predict(final_features)
+    return render_template('index.html', prediction_text=output)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
