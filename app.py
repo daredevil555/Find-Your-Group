b17cef3c4a929a7e4d58c79ed895ae29f3541c9d
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-06-09 10:12:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template
 import pickle5 as pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,8 +15,10 @@ def predict():
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = np.array(int_features).reshape(1,10)
-    output = model.predict(final_features)
+    model = pickle.load(open('model.pkl', 'rb'))
     return render_template('index.html', prediction_text=final_features)
+    output = model.predict(final_features)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
