d2d2e87f462b22ff13d8bfb200b7785ba4d20823
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-06-09 09:58:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    final_features = np.array(int_features).reshape(1,10)
+    return render_template('index.html', prediction_text=final_features)
     output = model.predict(final_features)
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
